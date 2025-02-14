2d72afdab5e420bfd8e65685c065bd8870d4d396
Murilo Sanches <murilo.de.paula2011@gmail.com>
2025-02-14 17:58:33 -0300

    Add log to connection and testing

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -5,15 +5,13 @@ dotenv.config();
 
 const connectionString = process.env.DATABASE_URL;
 
+console.log(connectionString)
+
 if (!connectionString) {
   throw new Error("⚠️ DATABASE_URL não definida no .env");
 }
 
 const sql = postgres(connectionString, {
-  ssl: {
-    rejectUnauthorized: false, 
-  },
-  timeout: 30 * 1000,
   idle_timeout: 60,
 });
 
