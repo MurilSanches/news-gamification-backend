53aa3475dc6140b363cc2e55ecbc60418f3062f4
Murilo Sanches <murilo.de.paula2011@gmail.com>
2025-02-14 18:06:18 -0300

    remove log and add ssl reject

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -5,13 +5,14 @@ dotenv.config();
 
 const connectionString = process.env.DATABASE_URL;
 
-console.log(connectionString)
-
 if (!connectionString) {
   throw new Error("⚠️ DATABASE_URL não definida no .env");
 }
 
 const sql = postgres(connectionString, {
+  ssl: {
+    rejectUnauthorized: false, 
+  },
   idle_timeout: 60,
 });
 
